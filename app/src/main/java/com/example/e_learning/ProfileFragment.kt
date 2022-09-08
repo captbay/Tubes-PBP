0879af4a9d1adb9b298aed24b24dc510a118817d
Alfonsinfo <itsmealfons@gmail.com>
2022-09-08 23:37:32 +0700

    Log Out Bisa

--- app/src/main/java/com/example/e_learning/ProfileFragment.kt
+++ app/src/main/java/com/example/e_learning/ProfileFragment.kt
@@ -1,15 +1,12 @@
 package com.example.e_learning
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.app.AlertDialog
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ProfileFragment : Fragment() {
@@ -19,28 +16,51 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile,container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val LogoutButton : FloatingActionButton = view.findViewById(R.id.floatingActionLogout)
+        val LogoutButton: FloatingActionButton = view.findViewById(R.id.floatingActionLogout)
+        val logout = Intent(this.getActivity(), LoginActivity::class.java)
 
-        LogoutButton.setOnClickListener {
-//            val alertDialog : AlertDialog = AlertDialog.Builder(context
+//        LogoutButton.setOnClickListener {
+//            val alertDialog : AlertDialog = AlertDialog.Builder(this.requireActivity()!!.getApplicationContext()).create()
 //            alertDialog.setTitle("Log out Dialog")
 //            alertDialog.setMessage("Do You Want Log Out??")
-
-//            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "YES")
-//            {
-//            }
-//            alertDialog.setButton(AlertDialog, DialogInterface.BUTTON_POSITIVE, "NO")
-//            {
-//                dialog, which -> dialog.dismiss()
-//            }
+//
+////            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "YES")
+////            {
+////                dialog, which -> startActivity(logout)
+////                dialog.dismiss()
+////            }
+////            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "NO")
+////            {
+////                dialog, which ->
+////                dialog.dismiss()
+////            }
 //            alertDialog.show()
+//        }
+        LogoutButton.setOnClickListener(){
+            val builder1 = AlertDialog.Builder(
+                this.requireContext()
+            )
+            builder1.setMessage("Anda Yakin Ingin Meninggal Aplikasi ini ? ")
+            builder1.setCancelable(true)
+
+            builder1.setPositiveButton(
+                "Yes"
+            ) { dialog, id -> startActivity(logout) }
+
+            builder1.setNegativeButton(
+                "No"
+            ) { dialog, id -> dialog.cancel() }
+
+            val alert11 = builder1.create()
+            alert11.show()
         }
 
     }
 
 }
+
