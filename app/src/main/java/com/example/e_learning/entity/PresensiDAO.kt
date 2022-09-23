4d1103aa1aa72dd83145e91f9c9e48fbe96e1010
captbay <agespramana9@gmail.com>
2022-09-23 23:14:19 +0700

    Revert "Revert "memasukan CRUD untuk Presensi bagian kelas""
    This reverts commit ae3ac8bf9c07a0c4c001a03df7f4af7bba407c45.

---
+++ app/src/main/java/com/example/e_learning/entity/PresensiDAO.kt
@@ -0,0 +1,25 @@
+package com.example.e_learning.entity
+
+import androidx.room.*
+
+
+@Dao
+interface PresensiDAO {
+
+    @Insert
+    suspend fun addPresensi(presensi: Presensi)
+
+    @Update
+    suspend fun updatePresensi(presensi: Presensi)
+
+    @Delete
+    suspend fun deletePresensi(presensi: Presensi)
+
+    @Query("SELECT * FROM Presensi")
+    suspend fun getPresensi() : List<Presensi>
+
+    @Query("SELECT * FROM Presensi Where id =:presensi_id")
+    suspend fun getPresensi(presensi_id : Int) : List<Presensi>
+}
+
+//
