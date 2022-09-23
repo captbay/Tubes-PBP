1ce2792728206ee43ae6281d4896d26b86fd512d
captbay <agespramana9@gmail.com>
2022-09-23 22:53:32 +0700

    update database, profile, uodate profile
    semua dikerjakan bersama ages, arve, dan alfon di laptop ages kaka, biar tidak conflict (kita buat bareng ko di kosan)

--- app/src/main/java/com/example/e_learning/ProfileFragment.kt
+++ app/src/main/java/com/example/e_learning/ProfileFragment.kt
@@ -1,28 +1,60 @@
 package com.example.e_learning
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.e_learning.databinding.FragmentProfileBinding
+import com.example.e_learning.entity.ProfileDB
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
+    val db by lazy { activity?.let { ProfileDB(it) } }
+    private val id = "idKey"
+    private val myPreference = "myPref"
+    var sharedPreferences: SharedPreferences? = null
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val LogoutButton: FloatingActionButton = view.findViewById(R.id.floatingActionLogout)
         val logout = Intent(this.getActivity(), LoginActivity::class.java)
+
+        sharedPreferences = activity?.getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val profile = db?.profileDAO()?.getProfile(sharedPreferences!!.getString(id,"")!!.toInt())?.get(0)
+            binding.username.setText(profile?.username)
+            binding.password.setText(profile?.password)
+            binding.email.setText(profile?.email)
+            binding.noTelp.setText(profile?.noTelp)
+            binding.tglLahir.setText(profile?.tglLahir)
+
+            binding.floatingActionUpdate.setOnClickListener {
+                val moveEdit = Intent(activity, UpdateProfile::class.java)
+                startActivity(moveEdit)
+                activity?.finish()
+            }
+        }
 
         LogoutButton.setOnClickListener(){
             val builder1 = AlertDialog.Builder(
--- app/src/main/java/com/example/e_learning/UpdateProfile.kt
+++ app/src/main/java/com/example/e_learning/UpdateProfile.kt
@@ -1,11 +1,76 @@
 package com.example.e_learning
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.e_learning.databinding.ActivityUpdateProfileBinding
+import com.example.e_learning.entity.Profile
+import com.example.e_learning.entity.ProfileDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class UpdateProfile : AppCompatActivity() {
+    val db by lazy { ProfileDB(this) }
+    private lateinit var binding: ActivityUpdateProfileBinding
+    private val id = "idKey"
+    private val myPreference = "myPref"
+    var sharedPreferences: SharedPreferences? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update_profile)
+        getSupportActionBar()?.hide()
+        binding = ActivityUpdateProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+        val id = sharedPreferences!!.getString(id,"")!!.toInt()
+        loadData(id)
+
+        binding.buttonUpdate.setOnClickListener { menuItem->
+            when(menuItem.id){
+                R.id.button_update -> {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val Profile = db?.profileDAO()?.updateProfile(
+                            Profile(id,binding.editUsername.text.toString()
+                            ,binding.editPassword.text.toString(),binding.editEmail.text.toString(),
+                            binding.editTglLahir.text.toString(),binding.editNoTelp.text.toString())
+                        )
+                    }
+                    finish()
+                    val intent = Intent(this, ProfileFragment::class.java)
+                    val bundle = Bundle()
+                    bundle.putString("key","iniTerisi")
+                    intent.putExtra("keyBundle",bundle)
+                    startActivity(intent)
+
+                    true
+                }
+                else -> false
+            }
+
+        }
+
+
+
+
+    }
+
+    fun loadData(id: Int){
+        CoroutineScope(Dispatchers.IO).launch {
+            val Profile = db?.profileDAO()?.getProfile(id)?.get(0)
+
+
+            withContext(Dispatchers.Main){
+                binding.editUsername.setText(Profile?.username)
+                binding.editEmail.setText(Profile?.email)
+                binding.editNoTelp.setText(Profile?.noTelp)
+                binding.editTglLahir.setText(Profile?.tglLahir)
+                binding.editPassword.setText(Profile?.password)
+            }
+
+        }
     }
 }
