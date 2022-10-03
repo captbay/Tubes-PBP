0b6344cf535ed788bf35b642aabf10c11fe57197
Alfonsdev <itsmealfons@gmail.com>
2022-10-04 06:12:22 +0700

    Uncomment Beranda
    Uncomment Beranda, ubah referensi database dari todoDb ke ElearningDB

--- app/src/main/java/com/example/e_learning/Fragment/BerandaFragment.kt
+++ app/src/main/java/com/example/e_learning/Fragment/BerandaFragment.kt
@@ -1,20 +1,32 @@
 package com.example.e_learning.Fragment
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.e_learning.Data.ToDoList.ToDoList
-//import com.example.e_learning.Data.ToDoList.ToDoViewModel
-import com.example.e_learning.Data.ToDoList.TodoDB
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.e_learning.EditActivity
 import com.example.e_learning.TodoAdapter
+import com.example.e_learning.data.ELEARNINGDB
+import com.example.e_learning.data.todoList.Constant
+import com.example.e_learning.data.todoList.ToDoList
 import com.example.e_learning.databinding.FragmentBerandaBinding
+import kotlinx.android.synthetic.main.fragment_beranda.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 //import kotlinx.android.synthetic.main.fragment_beranda.*
 
 
 class BerandaFragment : Fragment() {
-    val db by lazy { activity?.let{TodoDB(it)} }
+    val db by lazy { activity?.let{ELEARNINGDB(it)} }
     private var _binding:FragmentBerandaBinding? = null
     private val binding get() = _binding!!
     lateinit var todoAdapter: TodoAdapter
@@ -29,46 +41,47 @@ class BerandaFragment : Fragment() {
     //Test
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        setupListener()
-//        setupRecyclerView()
+        setupListener()
+        setupRecyclerView()
     }
 
     private fun setupRecyclerView() {
-//        todoAdapter = TodoAdapter(arrayListOf(), object :
-//            TodoAdapter.OnAdapterListener {
-//            override fun onClick(toDoList: ToDoList) {
-//                intentEdit(toDoList.id, Constant.TYPE_READ)
-//            }
-//            override fun onUpdate(toDoList: ToDoList) {
-//                intentEdit(toDoList.id, Constant.TYPE_UPDATE)
-//            }
-//            override fun onDelete(toDoList: ToDoList) {
-//                deleteDialog(toDoList)
-//            }
-//        })
-//        rv_beranda.apply {
-//            layoutManager = LinearLayoutManager(requireContext().applicationContext)//application context in fragment?
-//            adapter = todoAdapter
-//        }
+        todoAdapter = TodoAdapter(arrayListOf(), object :
+            TodoAdapter.OnAdapterListener {
+            override fun onClick(toDoList: ToDoList) {
+                intentEdit(toDoList.id, Constant.TYPE_READ)
+            }
+            override fun onUpdate(toDoList: ToDoList) {
+                intentEdit(toDoList.id, Constant.TYPE_UPDATE)
+            }
+            override fun onDelete(toDoList: ToDoList) {
+                deleteDialog(toDoList)
+            }
+        })
+        rv_beranda.apply {
+            layoutManager = LinearLayoutManager(requireContext().applicationContext)//application context in fragment?
+            adapter = todoAdapter
+        }
     }
     private fun deleteDialog(toDoList: ToDoList){
-//        val alertDialog = AlertDialog.Builder(requireContext())
-//        alertDialog.apply {
-//            setTitle("Confirmation")
-//            setMessage("Are You Sure to delete this data From ${toDoList.judul}?")
-//            setNegativeButton("Cancel", DialogInterface.OnClickListener
-//            { dialogInterface, i -> dialogInterface.dismiss()
-//            })
-//            setPositiveButton("Delete", DialogInterface.OnClickListener
-//            { dialogInterface, i -> dialogInterface.dismiss()
-//                CoroutineScope(Dispatchers.IO).launch {
-//                    db!!.ToDoListDAO().deleteNote(toDoList)
-//                    loadData()
-//                }
-//            })
-//        }
-//        alertDialog.show()
+        val alertDialog = AlertDialog.Builder(requireContext())
+        alertDialog.apply {
+            setTitle("Confirmation")
+            setMessage("Are You Sure to delete this data From ${toDoList.judul}?")
+            setNegativeButton("Cancel", DialogInterface.OnClickListener
+            { dialogInterface, i -> dialogInterface.dismiss()
+            })
+            setPositiveButton("Delete", DialogInterface.OnClickListener
+            { dialogInterface, i -> dialogInterface.dismiss()
+                CoroutineScope(Dispatchers.IO).launch {
+                    db!!.ToDoListDAO().deleteToDo(toDoList)
+                    loadData()
+                }
+            })
+        }
+        alertDialog.show()
     }
+
     override fun onStart() {
         super.onStart()
         loadData()
@@ -76,26 +89,26 @@ class BerandaFragment : Fragment() {
     //untuk load data yang tersimpan pada database yang sudah create
 
     fun loadData() {
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val notes = db!!.ToDoListDAO().getNotes()
-//            Log.d("MainActivity","dbResponse: $notes")
-//            withContext(Dispatchers.Main){
-//                todoAdapter.setData( notes )
-//            }
-//        }
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db!!.ToDoListDAO().getNotes()
+            Log.d("MainActivity","dbResponse: $notes")
+            withContext(Dispatchers.Main){
+                todoAdapter.setData( notes )
+            }
+        }
     }
     fun setupListener() {
-//        button_create.setOnClickListener{
-//            intentEdit(0,Constant.TYPE_CREATE)
-//        }
+          button_create.setOnClickListener{
+          intentEdit(0, Constant.TYPE_CREATE)
+        }
     }
     //pick data dari Id yang sebagai primary key
-    fun intentEdit(noteId : Int, intentType: Int){
-//        startActivity(
-//            Intent(requireActivity().applicationContext, EditActivity::class.java)
-//                .putExtra("intent_id", noteId)
-//                .putExtra("intent_type", intentType)
-//        )
+    fun intentEdit(todoId : Int, intentType: Int){
+        startActivity(
+            Intent(requireActivity().applicationContext, EditActivity::class.java)
+                .putExtra("intent_id", todoId)
+                .putExtra("intent_type", intentType)
+        )
         }
 
     }
