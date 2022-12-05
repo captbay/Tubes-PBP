27e9a41e3e5ae346599d548641eebf2bdcfd138b
Alfonsdev <itsmealfons@gmail.com>
2022-12-05 22:25:04 +0700

    commmit

--- app/src/androidTest/java/com/example/e_learning/auth/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/e_learning/auth/LoginActivityTest.kt
@@ -1,37 +1,27 @@
 package com.example.e_learning.auth
 
 
-import androidx.test.espresso.DataInteraction
-import androidx.test.espresso.ViewInteraction
-import androidx.test.filters.LargeTest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-
-import androidx.test.InstrumentationRegistry.getInstrumentation
-import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
-
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
 import com.example.e_learning.R
-
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.Matchers.anything
-import org.hamcrest.Matchers.`is`
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -41,95 +31,192 @@ class LoginActivityTest {
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
 
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.CAMERA",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.MANAGE_EXTERNAL_STORAGE"
+        )
+
     @Test
     fun loginActivityTest() {
         val textInputEditText = onView(
-allOf(withId(R.id.loginUsername),
-childAtPosition(
-childAtPosition(
-withId(R.id.inputLayoutUsername),
-0),
-0),
-isDisplayed()))
-        textInputEditText.perform(replaceText("alfons"), closeSoftKeyboard())
-        
+            allOf(
+                withId(R.id.loginUsername),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayoutUsername),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText(""), closeSoftKeyboard())
+
         val materialButton = onView(
-allOf(withId(R.id.btnLogin), withText("Login"),
-childAtPosition(
-allOf(withId(R.id.linearLayout),
-childAtPosition(
-withId(R.id.loginLayout),
-3)),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.btnLogin), withText("Login"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout),
+                        childAtPosition(
+                            withId(R.id.loginLayout),
+                            3
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         materialButton.perform(click())
-        
-        val materialButton2 = onView(
-allOf(withId(R.id.btnClear), withText("clear text"),
-childAtPosition(
-allOf(withId(R.id.linearLayout),
-childAtPosition(
-withId(R.id.loginLayout),
-3)),
-0),
-isDisplayed()))
-        materialButton2.perform(click())
-        
-        val materialButton3 = onView(
-allOf(withId(R.id.btnLogin), withText("Login"),
-childAtPosition(
-allOf(withId(R.id.linearLayout),
-childAtPosition(
-withId(R.id.loginLayout),
-3)),
-1),
-isDisplayed()))
-        materialButton3.perform(click())
-        
+        onView(isRoot()).perform(waitFor(3000))
         val textInputEditText2 = onView(
-allOf(withId(R.id.loginUsername),
-childAtPosition(
-childAtPosition(
-withId(R.id.inputLayoutUsername),
-0),
-0),
-isDisplayed()))
-        textInputEditText2.perform(click())
-        
+            allOf(
+                withId(R.id.loginUsername),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayoutUsername),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(replaceText("alfons"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btnLogin), withText("Login"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout),
+                        childAtPosition(
+                            withId(R.id.loginLayout),
+                            3
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
         val textInputEditText3 = onView(
-allOf(withId(R.id.loginUsername),
-childAtPosition(
-childAtPosition(
-withId(R.id.inputLayoutUsername),
-0),
-0),
-isDisplayed()))
-        textInputEditText3.perform(replaceText("alfon"), closeSoftKeyboard())
-        
-        val textInputEditText4 = onView(
-allOf(withId(R.id.loginPassword),
-childAtPosition(
-childAtPosition(
-withId(R.id.inputLayoutPassword),
-0),
-0),
-isDisplayed()))
-        textInputEditText4.perform(replaceText("1q3"), closeSoftKeyboard())
-        
+            allOf(
+                withId(R.id.loginPassword),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayoutPassword),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText3.perform(replaceText("12345"), closeSoftKeyboard())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.btnLogin), withText("Login"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout),
+                        childAtPosition(
+                            withId(R.id.loginLayout),
+                            3
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton3.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+        pressBack()
+
         val materialButton4 = onView(
-allOf(withId(R.id.btnLogin), withText("Login"),
-childAtPosition(
-allOf(withId(R.id.linearLayout),
-childAtPosition(
-withId(R.id.loginLayout),
-3)),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.btnClear), withText("clear text"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout),
+                        childAtPosition(
+                            withId(R.id.loginLayout),
+                            3
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
         materialButton4.perform(click())
-        }
-    
+
+        val textInputEditText4 = onView(
+            allOf(
+                withId(R.id.loginUsername),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayoutUsername),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText4.perform(replaceText("alfonslagi"), closeSoftKeyboard())
+
+        val textInputEditText5 = onView(
+            allOf(
+                withId(R.id.loginPassword),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayoutPassword),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText5.perform(replaceText("1"), closeSoftKeyboard())
+        onView(isRoot()).perform(waitFor(3000))
+        pressBack()
+
+        val materialButton5 = onView(
+            allOf(
+                withId(R.id.btnLogin), withText("Login"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.linearLayout),
+                        childAtPosition(
+                            withId(R.id.loginLayout),
+                            3
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton5.perform(click())
+        onView(isRoot()).perform(waitFor(3000))
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -144,4 +231,20 @@ isDisplayed()))
             }
         }
     }
+
+    fun waitFor(delay: Long): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for " +delay+ "milliseconds"
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                uiController.loopMainThreadForAtLeast(delay)
+            }
+        }
     }
+}
