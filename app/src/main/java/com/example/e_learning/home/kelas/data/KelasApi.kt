9f0ed5261f4b7c3f92b57ac71338d679c56cf410
captbay <agespramana9@gmail.com>
2022-12-05 21:41:44 +0700

    apii dude

--- app/src/main/java/com/example/e_learning/home/kelas/data/KelasApi.kt
+++ app/src/main/java/com/example/e_learning/home/kelas/data/KelasApi.kt
@@ -2,7 +2,7 @@ package com.example.e_learning.home.kelas.data
 
 class KelasApi {
     companion object {
-        val BASE_URL = "https://elearning-pbp.herokuapp.com/"
+        val BASE_URL = "https://tubespbp.akordmusic.com/"
 
         val GET_ALL_URL = BASE_URL + "kelas"
         val GET_BY_ID_URL = BASE_URL + "kelas/"
--- app/src/main/java/com/example/e_learning/home/profile/ProfileApi.kt
+++ app/src/main/java/com/example/e_learning/home/profile/ProfileApi.kt
@@ -2,7 +2,7 @@ package com.example.e_learning.home.profile
 
 class ProfileApi {
     companion object {
-        val BASE_URL = "https://elearning-pbp.herokuapp.com/"
+        val BASE_URL = "https://tubespbp.akordmusic.com/"
 
         val GET_ALL_URL = BASE_URL + "users"
         val GET_BY_ID_URL = BASE_URL + "users/"
