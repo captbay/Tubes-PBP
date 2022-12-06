b86b98ee0007f8f705b5d743c66c48a364fcfd10
captbay <agespramana9@gmail.com>
2022-12-06 23:25:30 +0700

    Tugas udah mau

--- app/src/main/java/com/example/e_learning/home/tugas/TugasFragment.kt
+++ app/src/main/java/com/example/e_learning/home/tugas/TugasFragment.kt
@@ -1,34 +1,37 @@
 package com.example.e_learning.home.tugas
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.e_learning.R
+import com.example.e_learning.databinding.FragmentTugasBinding
+import com.example.e_learning.home.tugas.data.Tugas
+import com.example.e_learning.home.tugas.data.TugasAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TugasFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TugasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentTugasBinding? = null
+    private val binding get() = _binding!!
+    private var adapter: TugasAdapter? = null
+    private var queue: RequestQueue? = null
+    private var layoutLoading: LinearLayout? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +41,22 @@ class TugasFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tugas, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TugasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TugasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    companion object{
+        const val LAUNCH_ADD_ACTIVITY = 123
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val adapter : TugasAdapter = TugasAdapter(Tugas.litsOfTugas)
+
+        val rvTugas : RecyclerView = view.findViewById(R.id.rv_tugas)
+
+        rvTugas.layoutManager = layoutManager
+
+        rvTugas.setHasFixedSize(true)
+
+        rvTugas.adapter = adapter
+    }
+
 }
